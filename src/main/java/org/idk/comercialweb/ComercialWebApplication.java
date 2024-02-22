553d6eb191a52bb2ad761339949748008c594a99
LDT <ledientai16@gmail.com>
2024-02-22 23:08:11 +0700

    Init project

---
+++ src/main/java/org/idk/comercialweb/ComercialWebApplication.java
@@ -0,0 +1,13 @@
+package org.idk.comercialweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercialWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComercialWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/idk/comercialweb/ComercialWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.idk.comercialweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercialWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
